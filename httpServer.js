46e67ce322074537ba253408d86987cbbb06e6c4
vgybalo <vgybalo@ukr.net>
2019-07-22 00:25:33 +0300

    sixth commit

--- httpServer.js
+++ httpServer.js
@@ -6,26 +6,27 @@ const path = require('path');
 const server = http.createServer((req, res) => {
     });
 
-server.listen(3000,'127.0.0.1');
-let date = String(new Date());
-let cpu = os.cpus();
-let hostname = os.hostname();
-let memory = os.totalmem();
+server.listen(3000,'127.0.0.1', function () {
+    let date = String(new Date());
+    let cpu = os.cpus();
+    let hostname = os.hostname();
+    let memory = os.totalmem();
 
-let userCompInfo = 'CPU-' + cpu.model + ' ' + 'hostname-'+hostname + ' ' + 'memory-' + memory+ '\n';
+    let userCompInfo = 'CPU-' + cpu.model + ' ' + 'hostname-'+hostname + ' ' + 'memory-' + memory+ '\n';
 
 
 
-let filename = 'log' + '-' + date[4] +date[5] +date[6] + '-' + date[8] +date[9] + '-' + date[11] +date[12] + date[13] +
-date[14] + '-' + date[16] +date[17] + '-' + date[19] +date[20] + '-' + date[22] +date[23] +'.log';
-console.log(filename);
+    let filename = 'log' + '-' + date[4] +date[5] +date[6] + '-' + date[8] +date[9] + '-' + date[11] +date[12] + date[13] +
+        date[14] + '-' + date[16] +date[17] + '-' + date[19] +date[20] + '-' + date[22] +date[23] +'.log';
+    console.log(filename);
 
-fs.mkdir('lounchLogs', function(err) {
-    fs.writeFile(path.join(__dirname,'lounchLogs', filename), userCompInfo,
+
+    fs.writeFile(path.join(__dirname,'launchLogs', filename), userCompInfo,
         (err)=>{
             if(err) {
                 throw err;
             }
         }
-    )   
-  });
+    )
+
+});
