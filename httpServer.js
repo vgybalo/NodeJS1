ff2b70ba53af794f82b2ab7257c4f19a38a5f1ee
vgybalo <vgybalo@ukr.net>
2019-07-21 19:01:32 +0300

    second commit

--- httpServer.js
+++ httpServer.js
@@ -1,72 +1,15 @@
 const http = require('http');
 const fs = require('fs');
-const pass = require('path')
-
 
 const server = http.createServer((req, res) => {
-        let filePath = path.join (__dirname,'public', req.url === '/' ? index.html :req.url)
-        if (req.url === '/') {
-            fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, data) => {
-                    res.writeHead(200, {"Content-Type" : "text/html"});
-                    res.end(data);
-            })
-        }
-        else if (req.url === '`index/') {
-            fs.readFile(__dirname + '/public/index.html', 'utf-8', (err, data) => {
-                    res.writeHead(200, {"Content-Type" : "text/html"});
-                    res.end(data);
-            })
-        }
-        else if (req.url === '/about') {
-    fs.readFile(__dirname + '/public/about.html', 'utf-8', (err, data) => {
-                    res.writeHead(200, {"Content-Type" : "text/html"});
-                    res.end(data);
-            })
 
-    }
-         else if (req.url === '/contact') {
-    fs.readFile(__dirname + '/public/contact.html', 'utf-8', (err, data) => {
-                    res.writeHead(200, {"Content-Type" : "text/html"});
-                    res.end(data);
-            })
+});
 
-    }
-        else {
-            fs.readFile(__dirname + '/public/' + req.url, 'utf-8', (err, data) => {
-            res.end(data);
-             })
-        }
-       
+server.listen(3000,'127.0.0.1');
+let date = new Date();
 
-    /*if (req.url === '/') {
-
-        fs.readFile(__dirname+'/public/index.html', 'utf-8', (err, data) => {
-        res.writeHead(200, {"Content-Type" : "text/html"});
-        res.end(data);
-
-
-
-        })
-    /*res.writeHead(200, {"Content-Type" : "text/plain"});
-    res.end ('123');*/
-
-    /*}
-    else if (req.url === '/about') {
-    res.writeHead(200, {"Content-Type" : "text/plain"});
-    res.end ('here is about');
-
-    }
-    else {
-    res.writeHead(404, {"Content-Type" : "text/html"})
-      res.end ('<h3>try run / </h3><h1>404</h1>');  
-
-    }*/
-
-
-})
-
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT,() => {
-    console.log('listening')
-})
+//console.log(date);
+fs.appendFile('lounch.logs', date +'\n', function (err) {
+  if (err) throw err;
+  console.log(date);
+});
