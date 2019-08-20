04ce9ec5e8e4135017549967fabf3d6add905c55
vgybalo <vgybalo@ukr.net>
2019-08-20 13:42:44 +0300

    twelveth commit

--- routes/cars.js
+++ routes/cars.js
@@ -4,8 +4,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {//установлює адресу передачi даних
-  res.render('cars', { titleCar: 'Cars'}); 
+  res.render('cars', { titleCar: 'cars'}); 
   res.send(car);  
+  console.log(cars);
+  console.log(car);
+
 });
 
 module.exports = router;
