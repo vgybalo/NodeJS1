4854394468b9d6fa81b21af4fcbd040cbba2b8b8
vgybalo <vgybalo@ukr.net>
2019-09-17 13:38:31 +0300

    nineteenth commit

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,34 @@ router.get('/', function(req, res, next) {
 });
 
 /* Post home page. Send data*/
-router.post('/', function(req) {
+router.post('/', function(req, res) {
   
-  car.push(req.body);
-  console.log(car);
-let car_check = JSON.parse(req.body);
-console.log(car_check.model);
+  
+let car_model = req.body.model;
+let car_price = req.body.price;
+
+if (typeof(car_model) === "string") {
+
+  if (car_model.length > 0 ) {
+    if (typeof(car_price) === "string") {
+
+  if (Number(car_price) > 0 ) {
+    car.push(req.body);
+    console.log(car);
+  }
+  else if (Number(car_price)>1000000000) {
+    console.log ("You input very big number")
+  }
+  else console.log ("You don't input a number")
+}
+else console.log ("You input not a number")
+  }
+  else if (car_model.length>256) {
+    console.log ("You input very long text")
+  }
+  else console.log ("You don't input a text")
+}
+else console.log ("You input not a text")
   /*let car_check = JSON.parse(req.body, (data)=> {
     console.log(data);
   });*/
