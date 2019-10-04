42aadb1de37b76c6a1e2d470f6ba19f42a214a4a
vgybalo <vgybalo@ukr.net>
2019-10-04 17:08:04 +0300

    twenty third commit

--- models/trip.js
+++ models/trip.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 
 const TripSchema = new mongoose.Schema({
-  fromName: String,
-  toName: String,
-  departAt: String,
-  vehicle: String,
-
-
+  fromName: {
+    type: String,
+    required: true
+  },
+  toName: {
+    type: String,
+    required: true
+  },
+  departAt: {
+    type: String,
+    required: true
+  },
+  vehicle: {
+    type: String,
+    required: true
+  }
 });
 
 const Model = mongoose.model('Trip', TripSchema);
--- routes/api.js
+++ routes/api.js
@@ -1,15 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const path = require('path');
-const fs = require('fs');
 
-const tripModel = require('../models/trip');
-
-//const tripData = require('../data/trip');
-//const bodyParser = require("body-parser");
-//const jsonParser = bodyParser.json();
-//const urlencodedParser = bodyParser.urlencoded({extended: true});
-
+const urlencodedParser = bodyParser.urlencoded({extended: false});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -19,77 +11,22 @@ router.get('/', function(req, res, next) {
         });
 });
 
-let tripData = [
-  {
-    "fromName": "Berlin, Germany",
-    "toName": "Kyiv, Ukraine",
-    "departAt": "2019-05-29T00:00:00.000Z",
-    "vehicle": "plane"
-  },
-  {
-    "fromName": "Berlin, Germany",
-    "toName": "Dnipro, Ukraine",
-    "departAt": "2019-06-02T00:00:00.000Z",
-    "vehicle": "car"
-  },
-  {
-    "fromName": "London, UK",
-    "toName": "Kyiv, Ukraine",
-    "departAt": "2019-06-07T00:00:00.000Z",
-    "vehicle": "plane"
-  },
-  {
-    "fromName": "Lyon, France",
-    "toName": "Kyiv, Ukraine",
-    "departAt": "2019-06-07T00:00:00.000Z",
-    "vehicle": "plane"
-  },
-  {
-    "fromName": "Moscow, Russia",
-    "toName": "Kyiv, Ukraine",
-    "departAt": "2019-06-08T00:00:00.000Z",
-    "vehicle": "car"
-  },
-  {
-    "fromName": "Kyiv, Ukraine",
-    "toName": "Berlin, Germany",
-    "departAt": "2019-05-30T00:00:00.000Z",
-    "vehicle": "train"
-  }
-]
-//console.log(tripData[1]);
-//router.post("/addAllTrip", function (request, response) {
-    //if(!request.body) return response.sendStatus(400);
-         
 
-tripDataLength = tripData.length;
-    for( let i=0; i<tripDataLength; i++) {
-       console.log(tripData[i].fromName);
-        const trip = new tripModel({
-            fromName: tripData[i].fromName,
-            toName: tripData[i]. toName,
-            departAt: tripData[i]. departAt,
-            vehicle: tripData[i].vehicle
-        });
-        
-        trip.save();
-        //res.send('Ok');
-   }
-//});
-
-/*router.post("/add", function (request, response) {
-    //if(!request.body) return response.sendStatus(400);
+router.post("/", function (request, response) {
+    if(!request.body) return response.sendStatus(400);
     
-     /*const trip = new TripModel({
-        title: request.body.title,
-        content: request.body.content
-    });
-    trip.save();
-    res.send('Ok');
+     let fromNameList = req.query.fromName;
+    const trips = tripModel.find({"fromName" : fromNameList})
+        .then(data => {
+        res.send(data)
+        });
+    
+    res.send(fromNameList);
    
-});*/
+});
 
 
+console.dir(req.query.fromName)
 
 
 module.exports = router;
