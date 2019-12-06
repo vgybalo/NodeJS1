f531a76cda7a1fdf42e8dd3b5f86d6ce14e950e9
vgybalo <vgybalo@ukr.net>
2019-12-06 13:51:24 +0200

    fourty  fourth commit sokets & puppoteer

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
+const puppeteer = require('puppeteer');
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
 
+let scrape = async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://dou.ua/lenta/digests/');
+    await page.waitFor(10000);
+  
+    await page.click('body > div.g-page > div.l-content.m-content > div > div.col70.m-cola > div > div > div.col50.m-cola > div.b-lenta > article:nth-child(1) > h2 > a');
+    await page.waitFor(5000);
+    const result = await page.evaluate(() => {
+        let article = page.$$eval('article', article => article.content);
+        return article;
+        
+    });
+
+  browser.close();
+  console.log(result.h1);
+  return result;
+  
+};
+
+scrape().then((value) => {
+    console.log(value); // Получилось!
+});
+
+
+
+//socket server on
 app.listen(3000);
 
 function handler (req, res) {
@@ -11,43 +39,21 @@ function handler (req, res) {
       res.writeHead(500);
       return res.end('Error loading index.html');
     }
-
     res.writeHead(200);
     res.end(data);
   });
 }
-let id = [];
+
 io.on('connection', function (socket) {
   socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-       socket.broadcast.emit('input',data.my+' зашел <br />');
-       socket.emit('input',"Вы " + data.my+' зашел <br />');
-       id.push([socket.id, data.my]);
-       console.log(id);
-       
-    //console.log(data);
-  });
+  
   socket.on('new-msg', function (data) {     
-      socket.emit('msg', {msg: data.msg, name: data.name});
-      socket.broadcast.emit('msg', {msg: data.msg, name: data.name});
+      socket.emit('msg', result);
+      
         //console.log(data.msg);
        // console.log(data.name);
   });
 
-let userOutName = null;
-    socket.on('disconnect', function(data) {
-        
-        /*for(let i = 0; i < id.length; i++){
-            
-            console.log(id[1]);
-            if (id[0]===socket.id) {
-                userOutName= id[1];
-                console.log(id[1]);
-            }
-        }*/
-        userOutName = socket.id
-		socket.broadcast.emit('output',userOutName+' вышел <br />');
-        socket.emit('output',userOutName+' вышел <br />');
-         console.log(userOutName);
-	});
+
+	
 });
